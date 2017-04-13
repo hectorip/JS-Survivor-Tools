5beb2b6ac08f4afea5e10224c190e0bf428c8746
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2017-04-12 23:57:41 -0500

    combinador de identidad

--- ES6/basics.js
+++ ES6/basics.js
@@ -8,5 +8,16 @@
     return 0;
 }); // Arrow function con cuerpo de bloque, hay que ser explícitos en el valor que se regresa.
 
-((multiplicador) => (factor) => multiplicador * factor)(2)(5)
+((multiplicador) => (factor) => multiplicador * factor)(2)(5) // 10
 
+// Shadowing variables
+
+((multiplicador) => 
+    (factor) => 
+        (multiplicador) =>
+            multiplicador * factor)(2)(3)(4) // ¿Qué multiplicador se toma en cuenta? El primero o el segundo?
+
+// 12 => La segunda variable multiplicador "eclipsa" la que está en el scope superior
+
+((x) =>
+    (y) => x)(2)(4) // Combinador de identidad
