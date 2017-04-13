7559f4f0b376ce80d575f98f76de50f14c8eea57
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2017-04-13 10:00:38 -0500

    const

--- ES6/const.js
+++ ES6/const.js
@@ -1,4 +1,19 @@
+// La palabra reservada 'const' hace un binding de
+// un nombre con un valor dentro de su contexto, aplicando las mismas
+// reglas de shadowing que el binding de los parámetros de funciones:
+// el nombre del contexto eclipsa los nombres superiores.
+
+const PI = 3;
 ((diametro) => {
     const PI = 3.14159265;
     return PI * diametro;
-})();
+})(5);
+
+// Re-asignación de valores const
+
+// Un valor asignado con 'const' no se puede re-asignar
+const PI = 3;
+((diametro) => {
+    PI = 3.14159265; // Error de compilación
+    return PI * diametro;
+})(5);
