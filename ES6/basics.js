c8d4f4a6852c4dbb1ce9aed0d747d328342d71fc
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2017-04-12 23:18:44 -0500

    IIFE's

---
+++ ES6/basics.js
@@ -0,0 +1,12 @@
+// Arrow functions
+
+// función que no recibe argumentos y devuelve 0
+() => 0; // Si es una expresión que representa un valor se devuelve automáticamente.
+(() => 0)(); // IIFE => Immediately Invoked Function Expression
+
+(() => {
+    return 0;
+}); // Arrow function con cuerpo de bloque, hay que ser explícitos en el valor que se regresa.
+
+((multiplicador) => (factor) => multiplicador * factor)(2)(5)
+
