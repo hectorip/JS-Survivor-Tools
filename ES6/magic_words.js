8568c68d5d562716671b1420bba86c233db9d1f1
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2017-04-17 23:59:01 -0500

    magic words

---
+++ ES6/magic_words.js
@@ -0,0 +1,14 @@
+// Las palabras mágicas 'this' y 'arguments' están presentes dentro de las 
+// funciones de JS, excepto en las arrow functions.
+
+
+// Arguments contiene TODOS los parámetros pasados a a la función, 
+// independientemente de cuántos se hayan declarado.
+
+(function(arg0){ return arguments[0]})('Parangaricutirimicuaro'); // => 'Parangaricutirimicuaro'
+
+((arg0) => arguments[0])('Parangaricutirimicuaro'); // Error de ejecución
+
+// Es un objeto muy parecido a un array
+
+(function(){ return arguments})('uno', 'uno', 'dos', 'tres', 'cinco');
