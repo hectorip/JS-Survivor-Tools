220fafdf3c5d446a38c7d688f7aa3996b5d01dd0
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2017-04-18 23:56:30 -0500

    explicación combinadores

--- ES6/combinadores.js
+++ ES6/combinadores.js
@@ -1,11 +1,7 @@
 // Cobinadores, como se enseñan en Javascript Allongé
-// Un combinador es una función que sólo usa funciones ya definidas,
-// para producir un resultado, o devuelve una nueva función.
 
 // Combinador K
 ((x) => (y) => x )(5) // => Devuelve el mismo valor después de ejecutar una función, útil por sus efectos secundarios
 
 // Combinador I
 ((x) => x)
-
-
