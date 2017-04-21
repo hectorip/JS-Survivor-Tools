63b5ec04655cef95e16ee58a9ca3c7c39288caaa
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2017-04-20 23:59:50 -0500

    funciones

---
+++ ES6/funciones.js
@@ -0,0 +1,14 @@
+// Diferencia entre declaración de funciones y expresión de función
+
+// Esta es una declaración de función
+// Es "levantada" se comporta como si hubiera sido declarada
+// al principio del archivo siempre. 
+function hacerAlgo(){
+    console.log("Esta función hace algo.");
+}
+
+// No hace un binding de nombre de función a menos que se cree
+// una variable que la contenga, y no es "levantada" (hoisted)
+const hacerOtraCosa = function hacerEsaOtraCosa(){
+    console.log("Esta función hace otra cosa");
+}
