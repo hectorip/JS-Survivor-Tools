981bd87f5e9461340ca79fc54174b71c7c9c8ddb
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2017-04-23 00:00:40 -0500

    tap revised

--- ES6/decoradores/tap.js
+++ ES6/decoradores/tap.js
@@ -2,6 +2,11 @@
 // La función Tap sirve para ejecutar una función sobre un valor
 // y recibir el mismo valor, para continuar trabajando con él.
 
-const tap = function(fn){
-    return (fn(val), val);
+const tap = function(value){
+    return function(fn){
+        (typeof(fn) === 'function'&& fn(value), val);
+    }
 }
+
+
+mi_valor = tap(console.log)
