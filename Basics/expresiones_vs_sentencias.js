99a9260d055e83a7202cf97a41ab446a10255bb4
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2017-04-29 23:59:44 -0500

    Expresiones vs Sentencias

---
+++ Basics/expresiones_vs_sentencias.js
@@ -0,0 +1,21 @@
+// En JS, y en muchos otros lenguajes, una expresión y una sentencia
+// se comportan diferente. Un ejemplo es la declaración de funciones
+// dentro como sentencia o como expresión.
+
+// Declaración como sentencia
+// Crea un binding o asignación del nombre "saludar"
+// a la función.
+
+function saludar(){
+    console.log("holi");
+} // se pude usar como (saludar());
+
+
+// Declaración como expresión
+// No crea una asignación del nombre de la función. 
+
+(function saludar_olvidada(){
+    console.log("HOliiii");
+});
+// no se puede usar, a menos que se guarde en una variable, 
+// todo lo que se usa entre paréntesis se considera una expresión.
