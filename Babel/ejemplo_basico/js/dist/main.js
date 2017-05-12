4821f6e7e651cb16c53504657a52521454870d91
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2017-05-12 10:34:01 -0500

    compilación con Babel

---
+++ Babel/ejemplo_basico/js/dist/main.js
@@ -0,0 +1,5 @@
+"use strict";
+
+var test_varible = "Hello ES6";
+var test_varible2 = "Hello ES6";
+var test_varible3 = "Hello ES6";
---
+++ Babel/ejemplo_basico/js/src/main.js
@@ -0,0 +1,3 @@
+let test_varible = "Hello ES6";
+let test_varible2 = "Hello ES6";
+let test_varible3 = "Hello ES6";
