f459180e55d919e9d68b75a84e7c763b48d57b6c
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2017-05-11 23:57:22 -0500

    starting with objects

---
+++ ES6/objects/shorthands.js
@@ -0,0 +1,31 @@
+// Shorthand en sitaxis de objetos literales
+
+
+let nombre = "Hemholtz", clase =  "A";
+// ES5
+var persona = {
+    nombre: nombre,
+    clase: clase
+}
+
+// ES6
+const persona_es6 = {
+    nombre,
+    clase
+} // cuando la clave se llama igual que la variable, basta con escribir el nombre de la variable
+
+// Métodos concisos
+
+var trabajador = {
+    nombre: nombre,
+    trabajar: function(){
+        console.log(this.nombre + " comenzó a trabajar");
+    }
+}
+
+const trabajador_six = {
+    nombre,
+    trabajar(){
+        console.log(`${ this.nombre } comenzó a trabajar`);
+    }
+}
