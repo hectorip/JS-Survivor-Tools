4b32fb26dbb8c7a7bab2f589bdadab01302d590a
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2017-05-17 23:43:57 -0500

    DEstructuring en arrays

---
+++ ES6/destructuring/arrays.js
@@ -0,0 +1,8 @@
+// Destructuring en arrays
+
+let series = [1, 2, 3, 4, 5, 6, 7];
+
+let [uno, dos, tres] = series;
+console.log(uno); // 1
+console.log(dos); // 2
+console.log(tres); // 3, el resto es ignorado
