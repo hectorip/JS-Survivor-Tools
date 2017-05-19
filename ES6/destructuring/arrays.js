edd09141ace092796bc1e9e6f79bb77857260d78
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2017-05-18 23:57:53 -0500

    Destructuración en arrays, más ejemplos

--- ES6/destructuring/arrays.js
+++ ES6/destructuring/arrays.js
@@ -10,6 +10,7 @@ console.log(tres); // 3, el resto es ignorado
 
 console.log("Destructuración sin ocupar todas las variables, y el capturando el sobrante: ");
 let [,,,cuatro] = series, [,,,,...resto] = series;
+// [...resto, cualquier_otra_cosa] = series; // error de sintaxis
 console.log(cuatro); // 4
 console.log(resto); // [5, 6, 7]
 
@@ -20,8 +21,9 @@ let matrix = [
 ];
 
 
-console.log("Destructuración anidada: ");
-let [ [uno_uno], [dos_uno], [tres_uno]  ] = matrix;
+console.log("Destructuración anidada y defaults: ");
+let [ [uno_uno], [dos_uno], [tres_uno], cuatro_uno = "10"  ] = matrix;
 console.log(uno_uno); // 1
 console.log(dos_uno); // 4
 console.log(tres_uno); // 7
+console.log(cuatro_uno); // 10
