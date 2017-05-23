ef4562b1d83b8c585c3d02c5962ec74e5822fecb
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2017-05-22 23:51:38 -0500

    Empezando explicación de símbolos bien conocidos

---
+++ ES6/symbols/well_known_symbols.js
@@ -0,0 +1,12 @@
+console.log(`
+#################################################
+Símbolos conocidos
+Con el objetivo de permitir cambiar algunas
+funciones que en ES5 parecían mágicas, ES6 ha
+hecho disponibles una serie de Symbols que pueden
+usarse para sobreescribir o acceder a comportami-
+entos default. Todos están disponibles a través
+del objeto Symbol. La referencia completa está
+en: https://developer.mozilla.org/en/docs/Web/JavaScript/Reference/Global_Objects/Symbol#Well-known_symbols
+#################################################
+`);
