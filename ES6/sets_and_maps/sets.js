f660b063cc73bed4be616fb05663d2024610a074
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2017-05-25 23:53:23 -0500

    Memory leak

--- ES6/sets_and_maps/sets.js
+++ ES6/sets_and_maps/sets.js
@@ -21,3 +21,18 @@ let mugiwara = new Set(["Luffy", "Zoro", "Ussop"]); // también pueden recibir u
 
 console.log(mugiwara);
 console.log(mugiwara.size);
+
+console.log(migiwara.has("Luffy"));
+console.log(migiwara.has("Sanji"));
+
+console.log(`
+###################################################
+Memory leak
+###################################################
+`);
+let personaje = {
+    nombre: "Tony Tony Chopper",
+    recompensa: 50
+}
+mugiwara.add(personaje);
+personaje = null; // la referencia se queda guardada y no se puede reciclar la memoria
