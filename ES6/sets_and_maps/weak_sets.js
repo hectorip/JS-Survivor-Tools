41648e619fed07ab6dc4c645c7a99e6842e462d1
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2017-05-30 18:50:36 -0500

    Weak sets, última referencia

--- ES6/sets_and_maps/weak_sets.js
+++ ES6/sets_and_maps/weak_sets.js
@@ -9,4 +9,12 @@ Sólo aceptan objetos.
 #################################################
 `);
 
-let mis_nakamas = new WeakSet([{nombre: "Luffy"}, {nombre: "Zoro"} ])
+let mis_nakamas = new WeakSet([{nombre: "Luffy"}, {nombre: "Zoro"}]);
+let tercer_nakama = {nombre: "Usopp"};
+mis_nakamas = mis_nakamas.add(tercer_nakama);
+mis_nakamas.has(tercer_nakama);
+tercer_nakama = null; // pierde la referencia
+
+// No hay manera de comprobar que se borró algo de un weak set si no se tiene
+// una referencia externa, como otra variable, pero eso
+// propiciaría que no se le aplicara garbage collection
