3bdb6d9ae7b7ea62f557bcf026f109887c623973
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2017-05-31 23:59:19 -0500

    empezando con mapas

---
+++ ES6/sets_and_maps/maps.js
@@ -0,0 +1,10 @@
+console.log(`
+#################################################
+Mapas
+Conjunto de claves y valores que permite usar 
+caualquier dato como llave.
+#################################################
+`);
+
+let un_mapa = new Map()
+un_mapa.add("key", 1)
