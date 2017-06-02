caecf037e1b9fcda08468a66b506a8799cd73253
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2017-06-02 14:18:34 -0500

    operaciones en maps

--- ES6/sets_and_maps/maps.js
+++ ES6/sets_and_maps/maps.js
@@ -15,3 +15,21 @@ console.log(un_mapa.has("key"));
 
 console.log(un_mapa.get("key"));
 console.log(un_mapa.get("key2"));
+
+// Las llaves no son forzadas en string
+let key_str = "5", key_number = 5;
+un_mapa.set(key_str, "Cinco");
+un_mapa.set(key_number, 4.99);
+
+console.log(`El mapa tiene la llave "5": ${un_mapa.has("5")}`);
+console.log(`El mapa tiene la llave 5 (número): ${un_mapa.has(5)}`);
+
+console.log(`El valor de la llave "5": ${un_mapa.get("5")}`);
+console.log(`El número de la llave 5(número): ${un_mapa.get(5)}`);
+
+// Size antes y después de clear
+console.log(un_mapa.size);
+un_mapa.clear();
+console.log(un_mapa.size);
+
+
