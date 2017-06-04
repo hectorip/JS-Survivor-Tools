ae866c4a09cf5fc3ec947e8742d03994299b2f80
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2017-06-03 23:56:22 -0500

    Simulando propiedades privadas

---
+++ ES6/sets_and_maps/simulating_private_properties/es5.js
@@ -0,0 +1,24 @@
+console.log(`
+#################################################
+Simulando propiedades privadas a la manera de ES5
+#################################################
+`);
+
+
+var Carro = function(){
+    var _id = 0, propiedades_privadas = {};
+
+    var Carro = function(modelo){
+        this._id = _id++;
+        propiedades_privadas[this._id] = {modelo: modelo};
+    }
+    Carro.prototype.getModelo = function(){
+        return propiedades_privadas[this._id].modelo;
+    }
+    return Carro;
+}(); // Aislamiento del contexto
+
+
+var ford = new Carro("Focus");
+console.log(ford.modelo); // no functiona : undefined
+console.log(ford.getModelo());
