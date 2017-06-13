d3f669e3df8371defb2bc910ca147c954f206864
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2017-06-12 23:59:39 -0500

    ecplicación shadowing

--- ES6/basics/shadowing.js
+++ ES6/basics/shadowing.js
@@ -1,12 +1,14 @@
-// Shadowing variables
+// Shadowing(sobreposición) de  variables
 
 ((multiplicador) => 
     (factor) => 
         (multiplicador) =>
-            multiplicador * factor)(2)(3)(4) // ¿Qué multiplicador se toma en cuenta? El primero o el segundo?
+            multiplicador * factor)(2)(3)(4) // ¿Qué multiplicador se toma en cuenta? ¿El primero o el segundo?
 
 // 12 => La segunda variable multiplicador "eclipsa" la que está en el scope superior
 
+// functiones equivalentes, se nota la diferencia gracias a la sintaxis simplificada de las
+// arrow functions
 const productorMultiplicadores = function(multiplicador){
     return(function(factor){
         return function(multiplicador){
