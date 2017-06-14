3c08b125638134567ac75352032b00d618789220
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2017-06-13 23:57:57 -0500

    iteradores en colecciones

--- ES6/iterators/basics.js
+++ ES6/iterators/basics.js
@@ -38,5 +38,24 @@ console.log(los_arcadios.next());
 console.log(los_arcadios.next()); // done
 
 
+console.log(`
+#################################################
+Coleeciones Iterables
+Las coleeciones en ES6 son iterables(array, map y
+set), porque poseen una función generador, en la 
+llave con el símbolo conocido Symbol.iterator.
+
+Para crear nuestras propios objetos iterables hay
+que crear esta misma propiedad.
+#################################################
+`);
+
+let numeros_primos = [1, 2, 3, 5, 7, 11];
+let iterador = numeros_primos[Symbol.iterator]();
+console.log(iterador.next()); // {value: 1, done: false}
+console.log(iterador.next()); // {value: 2, done: false}
+console.log(iterador.next()); // {value: 3, done: false}
+
+
 
 
